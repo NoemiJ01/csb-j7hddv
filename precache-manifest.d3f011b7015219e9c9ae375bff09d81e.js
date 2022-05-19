6197e616ab103bf4b7a1dbcf37efb720fb4bbdcf
Code Sandbox <hello@codesandbox.io>
2022-05-19 16:27:34 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d3f011b7015219e9c9ae375bff09d81e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe842145c631f8c0250283b32009e16",
+    "url": "https://noemij01.github.io/csb-j7hddv/index.html"
+  },
+  {
+    "revision": "dde7ae3ff4f2efe07c2a",
+    "url": "https://noemij01.github.io/csb-j7hddv/static/css/main.1e51bf68.chunk.css"
+  },
+  {
+    "revision": "e448ff7914277944ae9f",
+    "url": "https://noemij01.github.io/csb-j7hddv/static/js/2.c7c97cef.chunk.js"
+  },
+  {
+    "revision": "dde7ae3ff4f2efe07c2a",
+    "url": "https://noemij01.github.io/csb-j7hddv/static/js/main.5a89a08f.chunk.js"
+  },
+  {
+    "revision": "827bc0afc2ab27d6683e",
+    "url": "https://noemij01.github.io/csb-j7hddv/static/js/runtime~main.1efb5caf.js"
+  }
+]);
